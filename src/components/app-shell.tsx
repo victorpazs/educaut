be40f7102a28d09f35c4fecd8454b1df6f50c12b
victorpazs <victorps.2004@gmail.com>
2025-08-25 23:02:26 -0300

    feat: add sidebar and remove topbar items

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Topbar } from "@/components/topbar";
+import { Sidebar } from "./sidebar";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-background text-foreground">
+      <Sidebar />
       <Topbar />
-      <main className="mx-auto w-full max-w-6xl flex-1 p-4 md:p-6">
+      <main className="mx-auto w-full max-w-7xl flex-1 p-4 md:p-6">
         {children}
       </main>
     </div>
--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,47 +1,25 @@
 "use client";
 
-import { Search } from "lucide-react";
-import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import { Avatar } from "@/components/ui/avatar";
 import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function Topbar() {
   const pathname = usePathname();
 
-  const nav = [
-    { href: "/dashboard", label: "Dashboard" },
-    { href: "/students", label: "Alunos" },
-    { href: "/classes", label: "Turmas" },
-    { href: "/activities", label: "Atividades" },
-  ];
-
   return (
-    <header className="flex h-14 items-center gap-3 border-b border-border bg-background px-4">
-      <nav className="hidden items-center gap-1 md:flex">
-        {nav.map((item) => (
-          <Link
-            key={item.href}
-            href={item.href}
-            className={cn(
-              "rounded-md px-3 py-2 text-sm transition-colors",
-              pathname === item.href
-                ? "bg-primary/10 text-primary"
-                : "text-muted-foreground hover:bg-muted/60 hover:text-foreground"
-            )}
-          >
-            {item.label}
-          </Link>
-        ))}
+    <header className="border-b border-border bg-background px-8 py-1.5 ">
+      <nav className="flex justify-between items-center gap-3 max-w-[1800px] mx-auto ">
+        <Image
+          alt="Educaut logo"
+          width={50}
+          height={50}
+          src={"/app-logo.png"}
+        />
+        <div className="flex items-center gap-3">
+          <Avatar fallback="VP" />
+        </div>
       </nav>
-      <div className="relative ml-auto hidden w-full max-w-md items-center md:flex">
-        <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-        <Input className="pl-9" placeholder="Buscar..." />
-      </div>
-      <div className="ml-3 flex items-center gap-3">
-        <Avatar fallback="VP" />
-      </div>
     </header>
   );
 }
